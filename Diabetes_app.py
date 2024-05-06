a036be6fe0b279e5603cf4243ee0c6d88ec9c647
Anyanwu_Justice <105282903+Justin0207@users.noreply.github.com>
2024-05-06 13:30:01 +0100

    Update Diabetes_app.py

--- Diabetes_app.py
+++ Diabetes_app.py
@@ -32,7 +32,7 @@ def set_background(main_bg):
              background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
              background-size: cover;
          }}
-         .st-emotion-cache-bvjeak{{
+         .st-emotion-cache-15ndg0s{{
              background: None}}
          </style>
          """,
